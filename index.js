499dd2371945b826d80950747c73ea451f0da2f1
nathan <nathanmageka6@gmail.com>
2023-03-21 14:56:32 +0300

    nathan

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide() {
+    return 2000/100;
+}
+
+const square = (x)=>x*x;
+
+
+let sum=function(){
+    return 'add';
+}
+let a=3
+let b=4
+let add =(parameter1,parameter2) => parameter1 + parameter2;
+
